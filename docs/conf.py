535b9165f15e9279d0a28c9c242cf28f92e5308b
Ilan Steemers <koed00@gmail.com>
2015-07-04 21:59:37 +0200

    Larger logo for latex docs

--- docs/conf.py
+++ docs/conf.py
@@ -255,7 +255,7 @@ latex_documents = [
 
 # The name of an image file (relative to this directory) to place at the top of
 # the title page.
-latex_logo = 'logo.png'
+latex_logo = 'logo_large.png'
 
 # For "manual" documents, if this is true, then toplevel headings are parts,
 # not chapters.
