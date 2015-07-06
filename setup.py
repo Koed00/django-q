5c05c8ff76c7bc0841eb7bff04ce43dc7fa26701
Ilan Steemers <koed00@gmail.com>
2015-07-06 20:56:34 +0200

    Bugfix release 0.2.1.1

--- setup.py
+++ setup.py
@@ -26,7 +26,7 @@ class PyTest(Command):
 
 setup(
     name='django-q',
-    version='0.2.1',
+    version='0.2.1.1',
     author='Ilan Steemers',
     author_email='koed00@gmail.com',
     packages=['django_q'],
