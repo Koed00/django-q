9b58902243e13a29d97c47ff528751899f3b3bb1
Ilan Steemers <koed00@gmail.com>
2015-07-08 22:47:02 +0200

    Update setup.py
    Bumping to version 0.3.1

--- setup.py
+++ setup.py
@@ -26,7 +26,7 @@ class PyTest(Command):
 
 setup(
     name='django-q',
-    version='0.3.0',
+    version='0.3.1',
     author='Ilan Steemers',
     author_email='koed00@gmail.com',
     keywords='django task queue worker redis multiprocessing',
