82f9c4b44fdd7c6bfd6554df7cf9bbff002322c3
Ilan Steemers <koed00@gmail.com>
2015-07-08 18:46:08 +0200

    Getting ready for version 0.3.0

--- django_q/__init__.py
+++ django_q/__init__.py
@@ -1,6 +1,6 @@
 from .tasks import async, schedule, result, fetch
 from .models import Task, Schedule
 
-VERSION = (0, 2, 2)
+VERSION = (0, 3, 0)
 
 default_app_config = 'django_q.apps.DjangoQConfig'
