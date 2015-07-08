d9d0e7db73b5e84a83750e497ccb895f1dbc0449
Ilan Steemers <koed00@gmail.com>
2015-07-08 14:27:51 +0200

    consistent task name formatting

--- django_q/models.py
+++ django_q/models.py
@@ -57,12 +57,12 @@ def call_hook(sender, instance, **kwargs):
                 m = importlib.import_module(module)
                 f = getattr(m, func)
             except (ValueError, ImportError, AttributeError):
-                logger.error(_('malformed return hook \'{}\' for {}').format(instance.hook, instance.name))
+                logger.error(_('malformed return hook \'{}\' for [{}]').format(instance.hook, instance.name))
                 return
         try:
             f(instance)
         except Exception as e:
-            logger.error(_('return hook {} failed on {} because {}').format(instance.hook, instance.name, e))
+            logger.error(_('return hook {} failed on [{}] because {}').format(instance.hook, instance.name, e))
 
 
 class SuccessManager(models.Manager):
