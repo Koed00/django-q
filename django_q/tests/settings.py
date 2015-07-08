0a8196838ade27ec97bf23f698876b8070132f66
Ilan Steemers <koed00@gmail.com>
2015-07-08 21:02:00 +0200

    Removed security middleware from test for Django 1.7

--- django_q/tests/settings.py
+++ django_q/tests/settings.py
@@ -35,7 +35,6 @@ MIDDLEWARE_CLASSES = (
     'django.contrib.auth.middleware.SessionAuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
-    'django.middleware.security.SecurityMiddleware',
 )
 
 ROOT_URLCONF = 'tests.urls'
