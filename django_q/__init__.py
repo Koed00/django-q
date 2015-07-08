1375cece723b9627be1413f2eb25aa53b6a918ec
Ilan Steemers <koed00@gmail.com>
2015-07-08 22:48:25 +0200

    Update __init__.py
    Bumping to version 0.3.1

--- django_q/__init__.py
+++ django_q/__init__.py
@@ -1,6 +1,6 @@
 from .tasks import async, schedule, result, fetch
 from .models import Task, Schedule
 
-VERSION = (0, 3, 0)
+VERSION = (0, 3, q)
 
 default_app_config = 'django_q.apps.DjangoQConfig'
