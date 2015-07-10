937521666609ded12a8f0e9998792638cb6d947d
Ilan Steemers <koed00@gmail.com>
2015-07-10 12:38:02 +0200

    Bumping version to 0.3.3

--- django_q/__init__.py
+++ django_q/__init__.py
@@ -7,6 +7,6 @@ sys.path.insert(0, myPath)
 from .tasks import async, schedule, result, fetch
 from .models import Task, Schedule
 
-VERSION = (0, 3, 2)
+VERSION = (0, 3, 3)
 
 default_app_config = 'django_q.apps.DjangoQConfig'
