4a7feb8e502b6cbb78d319b550e062b8d8fb4ced
Ilan <koed00@gmail.com>
2015-07-15 23:14:37 +0200

    getting ready for new version

--- django_q/__init__.py
+++ django_q/__init__.py
@@ -8,6 +8,6 @@ from .tasks import async, schedule, result, fetch
 from .models import Task, Schedule, Success, Failure
 from .cluster import Cluster
 
-VERSION = (0, 3, 4)
+VERSION = (0, 3, 5)
 
 default_app_config = 'django_q.apps.DjangoQConfig'
