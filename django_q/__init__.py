b50e2a9006cc89f9edb6c055356aa39bba0bfadc
Ilan <koed00@gmail.com>
2015-08-01 15:55:21 +0200

    ready for 0.4.5

--- django_q/__init__.py
+++ django_q/__init__.py
@@ -9,6 +9,6 @@ from .models import Task, Schedule, Success, Failure
 from .cluster import Cluster
 from .monitor import Stat
 
-VERSION = (0, 4, 4)
+VERSION = (0, 4, 5)
 
 default_app_config = 'django_q.apps.DjangoQConfig'
