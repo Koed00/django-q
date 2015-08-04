77395acbbb5f52c22e5cbcfdc69b3092f4ebfeab
Ilan <koed00@gmail.com>
2015-08-04 21:45:51 +0200

    Preparing v0.4.6

--- setup.py
+++ setup.py
@@ -26,7 +26,7 @@ class PyTest(Command):
 
 setup(
     name='django-q',
-    version='0.4.5',
+    version='0.4.6',
     author='Ilan Steemers',
     author_email='koed00@gmail.com',
     keywords='django task queue worker redis multiprocessing',
