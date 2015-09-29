07a7851b54aa2448ab99d8f93d0734f757f85bd9
Ilan Steemers <koed00@gmail.com>
2015-09-29 22:59:40 +0200

    v0.7.7

--- django_q/__init__.py
+++ django_q/__init__.py
@@ -5,13 +5,13 @@ from django import get_version
 myPath = os.path.dirname(os.path.abspath(__file__))
 sys.path.insert(0, myPath)
 
-VERSION = (0, 7, 6)
+VERSION = (0, 7, 7)
 
 default_app_config = 'django_q.apps.DjangoQConfig'
 
 # root imports will slowly be deprecated.
 # please import from the relevant sub modules
-if get_version().split('.')[1][0] == '9':
+if get_version().split('.')[1][0] != '9':
     from .tasks import async, schedule, result, result_group, fetch, fetch_group, count_group, delete_group, queue_size
     from .models import Task, Schedule, Success, Failure
     from .cluster import Cluster
