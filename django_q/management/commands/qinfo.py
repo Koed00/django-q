d58c31e10351f4eb46e5d3a7e025bb4f58fb62e2
Ilan Steemers <koed00@gmail.com>
2016-06-07 16:43:51 +0200

    Replaces optparse with argparse for commands

--- django_q/management/commands/qinfo.py
+++ django_q/management/commands/qinfo.py
@@ -1,7 +1,4 @@
-from optparse import make_option
-
 from django.core.management.base import BaseCommand
-
 from django.utils.translation import ugettext as _
 
 from django_q import VERSION
@@ -13,13 +10,14 @@ class Command(BaseCommand):
     # Translators: help text for qinfo management command
     help = _('General information over all clusters.')
 
-    option_list = BaseCommand.option_list + (
-        make_option('--config',
-                    action='store_true',
-                    dest='config',
-                    default=False,
-                    help='Print current configuration.'),
-    )
+    def add_arguments(self, parser):
+        parser.add_argument(
+            '--config',
+            action='store_true',
+            dest='config',
+            default=False,
+            help='Print current configuration.',
+        )
 
     def handle(self, *args, **options):
         if options.get('config', False):
