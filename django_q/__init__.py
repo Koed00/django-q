67bd68d10f053f0ea616b0990ebdbecf67417a58
Ilan Steemers <koed00@gmail.com>
2016-06-07 16:43:30 +0200

    Recognize Django 1.10 versions

--- django_q/__init__.py
+++ django_q/__init__.py
@@ -11,7 +11,8 @@ default_app_config = 'django_q.apps.DjangoQConfig'
 
 # root imports will slowly be deprecated.
 # please import from the relevant sub modules
-if get_version().split('.')[1][0] != '9':
+split_version = get_version().split('.')
+if split_version[1][0] != '9' and split_version[1][:2]!='10':
     from .tasks import async, schedule, result, result_group, fetch, fetch_group, count_group, delete_group, queue_size
     from .models import Task, Schedule, Success, Failure
     from .cluster import Cluster
