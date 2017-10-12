67790740238eceffad210285f1b50cbc5e3ee562
ilan <koed00@gmail.com>
2017-10-12 15:52:49 +0200

    Bumps version to 0.8.1

--- setup.py
+++ setup.py
@@ -26,7 +26,7 @@ class PyTest(Command):
 
 setup(
     name='django-q',
-    version='0.8.0',
+    version='0.8.1',
     author='Ilan Steemers',
     author_email='koed0@gmail.com',
     keywords='django distributed task queue worker scheduler cron redis disque ironmq sqs orm mongodb multiprocessing rollbar',
