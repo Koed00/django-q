805390fb9ed961dc03632a71b53f9fe71b47541a
Ronald van Zon <ronald.vanzon@nedap.com>
2017-12-12 15:09:03 +0100

    Remove include for admin site urls

--- django_q/tests/urls.py
+++ django_q/tests/urls.py
@@ -1,6 +1,6 @@
-from django.conf.urls import include, url
+from django.conf.urls import url
 from django.contrib import admin
 
 urlpatterns = [
-    url(r'^admin/', include(admin.site.urls)),
+    url(r'^admin/', admin.site.urls),
 ]
