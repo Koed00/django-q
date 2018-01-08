81d4b36b1b7c9f05aa5ec4fd1241875f27e2ff04
ilan <koed00@gmail.com>
2018-01-08 09:57:20 +0100

    Cherry picking compatibility commits

--- django_q/tests/settings.py
+++ django_q/tests/settings.py
@@ -33,10 +33,11 @@ MIDDLEWARE_CLASSES = (
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
-    'django.contrib.auth.middleware.SessionAuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 )
+
+MIDDLEWARE = MIDDLEWARE_CLASSES
 
 ROOT_URLCONF = 'tests.urls'
 
