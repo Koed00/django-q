7c6c318860ad7deb32a916ba4b7b6d9dd3296ab1
ilan <koed00@gmail.com>
2018-03-13 17:43:04 +0100

    0.9.3

--- setup.py
+++ setup.py
@@ -26,7 +26,7 @@ class PyTest(Command):
 
 setup(
     name='django-q',
-    version='0.9.2',
+    version='0.9.3',
     author='Ilan Steemers',
     author_email='koed0@gmail.com',
     keywords='django distributed task queue worker scheduler cron redis disque ironmq sqs orm mongodb multiprocessing rollbar',
