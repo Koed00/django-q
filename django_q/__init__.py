15f807931532d34ff5b55aff76fa4011ff27922a
Ilan Steemers <koed00@gmail.com>
2018-08-05 14:21:41 +0200

    Removes root imports

--- django_q/__init__.py
+++ django_q/__init__.py
@@ -1,21 +1,6 @@
-# import os
-# import sys
-import django
-
-# myPath = os.path.dirname(os.path.abspath(__file__))
-# sys.path.insert(0, myPath)
-
-VERSION = (0, 9, 4)
+VERSION = (1, 0, 0)
 
 default_app_config = 'django_q.apps.DjangoQConfig'
 
-# root imports will slowly be deprecated.
-# please import from the relevant sub modules
-if django.VERSION[:2] < (1, 9):
-    from .tasks import async_task, schedule, result, result_group, fetch, fetch_group, count_group, delete_group, queue_size
-    from .models import Task, Schedule, Success, Failure
-    from .cluster import Cluster
-    from .status import Stat
-    from .brokers import get_broker
 
 __all__ = ['conf', 'cluster', 'models', 'tasks']
