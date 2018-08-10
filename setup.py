83de4c28c7e94f7f9fc3ab053b7d4daa5921fa46
Ilan Steemers <koed00@gmail.com>
2018-08-10 13:36:31 +0200

    Updates setup conf

--- setup.py
+++ setup.py
@@ -1,10 +1,10 @@
 import os
+from setuptools import setup, Command
 
 with open(os.path.join(os.path.dirname(__file__), 'README.rst')) as readme:
     README = readme.read()
 
 os.chdir(os.path.normpath(os.path.join(os.path.abspath(__file__), os.pardir)))
-from setuptools import setup, Command
 
 
 class PyTest(Command):
@@ -54,6 +54,7 @@ setup(
         'Programming Language :: Python :: 3.6',
         'Programming Language :: Python :: 3.7',
         'Topic :: Internet :: WWW/HTTP',
+        'Topic :: System :: Distributed Computing',
         'Topic :: Software Development :: Libraries :: Python Modules',
     ],
     entry_points={
