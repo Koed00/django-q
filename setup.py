309600a3123e127fd27e1adb365515ffadf16586
Ilan Steemers <koed00@gmail.com>
2018-08-19 17:22:38 +0200

    Removes Python 3,4 for now

--- setup.py
+++ setup.py
@@ -49,7 +49,6 @@ setup(
         'Operating System :: MacOS',
         'Programming Language :: Python',
         'Programming Language :: Python :: 3',
-        'Programming Language :: Python :: 3.4',
         'Programming Language :: Python :: 3.5',
         'Programming Language :: Python :: 3.6',
         'Programming Language :: Python :: 3.7',
