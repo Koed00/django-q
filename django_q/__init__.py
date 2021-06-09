410327bff4baf90aa218b51a61cb9b914de0fb59
Nurettin Onur TUĞCU <onur.tugcu@gmail.com>
2021-06-09 13:27:15 +0300

    Update __init__.py (#572)
    Fixes 
    ```
    RemovedInDjango41Warning: 'django_q' defines default_app_config = 'django_q.apps.DjangoQConfig'. Django now detects this configuration automatically. You can remove default_app_config.
    ```

--- django_q/__init__.py
+++ django_q/__init__.py
@@ -1,6 +1,8 @@
 VERSION = (1, 3, 8)
 
-default_app_config = "django_q.apps.DjangoQConfig"
+import django
 
+if django.VERSION < (3, 2):
+    default_app_config = "django_q.apps.DjangoQConfig"
 
 __all__ = ["conf", "cluster", "models", "tasks"]
